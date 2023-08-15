19dafedc914ac36a3ae138c4a6249359c5d1850e
Sobitandra <kartikghosh770@gmail.com>
2023-08-15 11:12:16 +0530

    15 Aug

--- lib/screens/check_out/delivery_address.dart
+++ lib/screens/check_out/delivery_address.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:get/get.dart';
-import 'package:get/route_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
